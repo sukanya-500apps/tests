b0d0af1214fcba99c2785fdcb4940ad850e1563f
sukanya <sukanya.gumparthi@500apps.com>
2022-04-20 18:46:22 +0530

    commit

--- src/components/20-04-2022/que4.vue
+++ src/components/20-04-2022/que4.vue
@@ -20,24 +20,4 @@ user:usersData,
 };
 </script>
 
-<!--<template>
-    <div>
-        <h1>This isQustion-4</h1>
-        <p v-for="user in user" :key="user.id">
-            {{user.name}}{{user.email}}
-        </p>
-    </div>
-</template>
-<script>
-import usersData from '../20-04-2022/users.json'
-export default{
-    name:"QuestioN4",
-    data() {
-        return{
-            user:usersData,
-        }
-        
-    },
-}
-</script>-->
 
