4281b94b425f80edc290848f6a6d3ce553f283ee
sukanya <sukanya.gumparthi@500apps.com>
2022-04-22 18:55:21 +0530

    commit

--- src/components/19-04-2022/axios.vue
+++ src/components/19-04-2022/axios.vue
@@ -16,7 +16,7 @@ export default {
   },
   methods: {
     fun() {
-    let response= axios.get("https://jsonplaceholder.typicode.com/todos/1")
+    let response= axios.get("https://jsonplaceholder.typicode.com/todos/")
 let mypromise=new Promise((myResolve,myReject)=>{
     myResolve(response);
     myReject("error");
