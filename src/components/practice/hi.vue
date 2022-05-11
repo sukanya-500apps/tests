95fbebec043a2969c4fd58b8f5fe4b6d204ab71a
sukanya <sukanya.gumparthi@500apps.com>
2022-05-11 19:44:01 +0530

    commit

--- src/components/practice/hi.vue
+++ src/components/practice/hi.vue
@@ -2,28 +2,21 @@
   <div style="text-align: center " >
        <b-tabs content-class="mt-3">
     <b-tab title="First" active>
-
         <center><b-card style="max-width: 20rem;" class="mb-2">
 <b-card-group>
 <b-form>
-
 <b-form-input type="text" placeholder="enter product name" v-model="val"></b-form-input><br>
 <b-form-input type="text" placeholder="enter price" v-model="val1"></b-form-input><br>
 <b-form-input type="text" placeholder="enter productcategory" v-model="val2"></b-form-input><br>
 <b-button variant="btn btn-primary btn-sm" @click="fun()">submit</b-button>
-
-
-
 </b-form>
 </b-card-group>
 </b-card></center><br>
-
             </b-tab>
     <b-tab title="Second"><p>I'm the second tab</p></b-tab>
     <b-tab title="Disabled" disabled><p>I'm a disabled tab!</p></b-tab>
   </b-tabs>
     <b-button v-b-toggle.sidebar-no-header align="centre">save</b-button>
-
     <b-sidebar id="sidebar-no-header" aria-labelledby="sidebar-no-header-title" no-header shadow >
       <template #default="{ hide }">
         <div class="p-3">
@@ -70,7 +63,6 @@ this.names.push({
 productname:this.val,
 price:this.val1,
 category:this.val2,
-
 });
 this.val = ""
 this.val1 = ""
@@ -81,7 +73,4 @@ this.val3 = ""
 }
 </script>
 <style scoped>
-
-
-
 </style>
